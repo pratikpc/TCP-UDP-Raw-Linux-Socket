ff3c3a566b598ecde75aa2c0a23d7994dd289da4
Pratik Chowdhury <pratikc@live.co.uk>
2021-06-28 22:19:28 +0530

    fixup: cleanup code

--- include/pc/network/tcp/TCP.hpp
+++ include/pc/network/tcp/TCP.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <memory>
+
 #include <sys/socket.h>
 
 namespace pc
@@ -14,6 +14,7 @@ namespace pc
          TCP(int socket) : socket(socket) {}
          TCP(TCP&& left) : socket(left.socket)
          {
+            // Leave socket in invalid state
             left.socket = -1;
          };
 
@@ -43,40 +44,33 @@ namespace pc
             if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
                throw std::runtime_error("Unable to set reusable");
          }
-         void* recv(std::size_t size, int flags = 0)
+         void* recv(std::size_t size, int flags = 0) const
          {
             char* output = new char[size];
             int   opt;
             if ((opt = ::recv(socket, output, size, flags)) == -1)
-            {
-               std::cerr << "read() failed: " << strerror(errno) << "\n";
                throw std::runtime_error("Unable to read data");
-            }
-            std::cout << "Opt = " << opt << "\n";
             if (opt == 0)
                return nullptr;
             return output;
          }
          template <typename T>
-         T recv(int flags = 0)
+         T recv(int flags = 0) const
          {
             return *((T*)recv(sizeof(T), flags));
          }
 
          std::size_t send(const void* msg, size_t const len, int flags = 0) const
          {
-            auto const sent = ::send(socket, msg, len, flags);
+            std::size_t const sent = ::send(socket, msg, len, flags);
             if (sent == -1)
-            {
-               std::cerr << "send() failed: " << strerror(errno) << "\n";
                throw std::invalid_argument("Unable to send");
-            }
             return sent;
          }
          template <typename T>
          std::size_t send(T& msg, int flags = 0) const
          {
-            return ::send(socket, (void*)(&msg), sizeof(msg), flags);
+            return send((const void*)(&msg), sizeof(msg), flags);
          }
 
          ~TCP()
