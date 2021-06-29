8a7090658f50934ae7bf736cefa86f7a093c8675
Pratik Chowdhury <pratikc@live.co.uk>
2021-06-29 09:08:15 +0530

    feat: add simple pair

---
+++ include/pc/network/SimplePair.hpp
@@ -0,0 +1,12 @@
+namespace pc
+{
+   namespace network
+   {
+      template <typename T1, typename T2>
+      struct SimplePair
+      {
+         T1 first;
+         T2 second;
+      };
+   } // namespace network
+} // namespace pc
