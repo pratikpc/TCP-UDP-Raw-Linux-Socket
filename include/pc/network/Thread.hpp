21edf6a378060e993e965070af87ca0c398780b0
Pratik Chowdhury <pratikc@live.co.uk>
2021-06-29 13:52:51 +0000

    fix: check for detach before cancel

--- include/pc/network/Thread.hpp
+++ include/pc/network/Thread.hpp
@@ -9,19 +9,22 @@ namespace pc
       class Thread
       {
          pthread_t threadId;
+         bool      detached;
 
        public:
-         Thread(void* (*start_routine)(void*), void* arg)
+         Thread(void* (*start_routine)(void*), void* arg) : detached(false)
          {
             pthread_create(&threadId, NULL, start_routine, arg);
          }
          bool detach()
          {
+            detached = true;
             return pthread_detach(threadId) == 0;
          }
          ~Thread()
          {
-            pthread_cancel(threadId);
+            if (!detached)
+               pthread_cancel(threadId);
          }
       };
    } // namespace network
