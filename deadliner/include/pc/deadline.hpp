3e62d1a10fcb4286439aa6dcf27a2056a8205926
Pratik Chowdhury <pratikc@live.co.uk>
2021-06-30 11:56:34 +0000

    feat: deadline use millis

--- deadliner/include/pc/deadline.hpp
+++ deadliner/include/pc/deadline.hpp
@@ -18,9 +18,10 @@ namespace pc
 
       std::ptrdiff_t front;
       std::ptrdiff_t rear;
+      timespec       specTime;
 
     public:
-      Deadline(std::size_t maxCount = 25, std::size_t maxTime = 13) :
+      Deadline(std::size_t maxCount = 25, std::size_t maxTime = 15*1000) :
           maxCount(maxCount), maxTime(maxTime), curSize(0), kill(false), queue(maxCount),
           front(-1), rear(-1)
       {
@@ -30,14 +31,15 @@ namespace pc
       {
          return kill;
       }
-      time_t getCurrentTime()
+      time_t getCurrentTimeMs()
       {
-         return time(NULL);
+         clock_gettime(CLOCK_MONOTONIC, &specTime);
+         return specTime.tv_nsec / 1.0e6;
       }
 
       Deadline& operator++()
       {
-         time_t curTime = getCurrentTime();
+         time_t curTime = getCurrentTimeMs();
          if ((front == 0 && rear == maxCount - 1) || (front == rear + 1))
          {
             if ((curTime - queue[front]) <= maxTime)
