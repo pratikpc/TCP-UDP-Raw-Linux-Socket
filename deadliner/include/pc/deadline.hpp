a0849fbf180a0bb80fd4a3e6933d88f0dca2d0c0
Pratik Chowdhury <pratikc@live.co.uk>
2021-06-30 15:10:53 +0000

    feat(deadline): cleanup algorithm code. move out

--- deadliner/include/pc/deadline.hpp
+++ deadliner/include/pc/deadline.hpp
@@ -38,9 +38,55 @@ namespace pc
                                  specTime.tv_nsec / 1.0e6);
       }
 
-      Deadline& operator++()
+      friend std::ostream& operator<<(std::ostream& os, Deadline& deadline)
+      {
+         if (deadline.front == -1)
+            return os;
+         // Function to display status of Circular Queue
+         std::size_t i = 0;
+         for (i = deadline.front; i != deadline.rear; i = (i + 1) % deadline.maxCount)
+            os << (deadline.queue[i] - deadline.queue[deadline.front]) << ", ";
+         os << (deadline.queue[i] - deadline.queue[deadline.front]);
+         return os;
+      }
+      void IgnoreLargeDifferences()
+      { // Optimize
+         // Ignore the very large differences
+         // Move ahead of differences we may
+         // not be interested in
+         // If the deadline is greater now
+         // The deadline would certainly be greater later
+         // So these parts can be easily ignored
+         while (true)
+         {
+            if (front == -1)
+               break;
+            if (front == rear)
+               break;
+            // If greater, we can ignore this value
+            // And move ahead
+            if ((queue[rear] - queue[(front + 1) % maxCount]) > maxTime)
+            {
+#ifdef DEBUG
+               std::cout << std::endl
+                         << "In loop " << front << " f : r " << rear << " : "
+                         << (queue[rear] - queue[front] - maxTime) << " : " << *this;
+#endif
+               front = (front + 1) % maxCount;
+            }
+            else
+               break;
+         }
+      }
+      Deadline& increment(std::size_t count)
       {
          std::ptrdiff_t curTime = getCurrentTimeMs();
+#ifdef DEBUG
+         std::cout << std::endl
+                   << "operator " << front << " f : r " << rear << " : "
+                   << (curTime - queue[front] - maxTime) << " : " << *this;
+
+#endif
          if ((front == 0 && rear == maxCount - 1) || (front == rear + 1))
          {
             if ((curTime - queue[front]) <= maxTime)
@@ -48,41 +94,17 @@ namespace pc
                kill = true;
                return *this;
             }
-            // Optimize
-            // Ignore the very large differences
-            // Move ahead of differences we may
-            // not be interested in
-            // If the deadline is greater now
-            // The deadline would certainly be greater later
-            // So these parts can be easily ignored
-            while (true)
-            {
-               if (front == -1)
-                  return *this;
-               if (front == rear)
-               {
-                  front = -1;
-                  rear  = -1;
-                  return *this;
-               }
-               // If greater, we can ignore this value
-               // And move ahead
-               if ((curTime - queue[(front + 1) % maxCount]) > maxTime)
-               {
-                  front = (front + 1) % maxCount;
-                  std::cout << std::endl
-                            << " In loop " << front << " f : r " << rear << " : "
-                            << (curTime - queue[front] - maxTime);
-               }
-               else
-                  break;
-            }
          }
          if (front == -1)
             front = 0;
          rear        = (rear + 1) % maxCount;
          queue[rear] = curTime;
+         IgnoreLargeDifferences();
          return *this;
+      }
+      Deadline& operator++()
+      {
+         return increment(1);
       }
 
       Deadline& MaxCount(std::size_t MaxCount)
