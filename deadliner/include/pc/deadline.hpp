a480d2eefd66625161c693768e11b3cc80114145
Pratik Chowdhury <pratikc@live.co.uk>
2021-07-01 08:38:29 +0000

    fix: check throttling in operator bool instead

--- deadliner/include/pc/deadline.hpp
+++ deadliner/include/pc/deadline.hpp
@@ -11,27 +11,27 @@ namespace pc
       std::ptrdiff_t maxCount;
       std::ptrdiff_t maxTime;
 
-      bool kill;
-
       std::vector<std::ptrdiff_t> queue;
 
       std::ptrdiff_t front;
       std::ptrdiff_t rear;
-      timespec       specTime;
 
     public:
       Deadline(std::size_t maxCount = 25, std::ptrdiff_t maxTime = 10 * 1000) :
-          maxCount(maxCount), maxTime(maxTime), kill(false), queue(maxCount), front(-1),
-          rear(-1)
+          maxCount(maxCount), maxTime(maxTime), queue(maxCount), front(-1), rear(-1)
       {
       }
 
       operator bool() const
       {
-         return kill;
+         std::ptrdiff_t curTime = getCurrentTimeMs();
+         if ((front == 0 && rear == maxCount - 1) || (front == rear + 1))
+            return ((curTime - queue[front]) <= maxTime);
+         return false;
       }
-      std::ptrdiff_t getCurrentTimeMs()
+      std::ptrdiff_t getCurrentTimeMs() const
       {
+         timespec specTime;
          clock_gettime(CLOCK_MONOTONIC, &specTime);
          return (std::ptrdiff_t)(((std::ptrdiff_t)specTime.tv_sec * 1000) +
                                  specTime.tv_nsec / 1.0e6);
@@ -39,17 +39,10 @@ namespace pc
 
       Deadline& increment()
       {
-
          std::ptrdiff_t curTime = getCurrentTimeMs();
          // If queue is full
          if ((front == 0 && rear == maxCount - 1) || (front == rear + 1))
          {
-            // Compare current time and time at front
-            if ((curTime - queue[front]) <= maxTime)
-            {
-               kill = true;
-               return *this;
-            }
             // Deque current value
             front = (front + 1) % maxCount;
          }
@@ -69,7 +62,7 @@ namespace pc
          maxCount = MaxCount;
          return *this;
       }
-      std::size_t MaxCount()
+      std::size_t MaxCount() const
       {
          return maxCount;
       }
