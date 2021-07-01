aebde0c93a7e5383c620ae8645d77ec89b2dc473
Pratik Chowdhury <pratikc@live.co.uk>
2021-07-01 13:13:58 +0000

    feat(pqpp): sample extract with default values

--- sample/pqpp/test/src/main.cxx
+++ sample/pqpp/test/src/main.cxx
@@ -38,7 +38,7 @@ int main()
          std::cerr << "Failed to insert data\n";
          return EXIT_FAILURE;
       }
-      std::cout << "\nData inserted";
+      std::cout << "Data inserted\n";
    }
    {
       pc::pqpp::IterateResult res =
@@ -50,6 +50,33 @@ int main()
                    << "priority = " << res[1] << "\n";
          ++res;
       }
+   }
+   // Select single client which exists
+   {
+      std::vector<const char*> params(2);
+      params[0]                   = "CLIENT-1";
+      params[1]                   = "45"; // DEFAULT VALUE. UNUSED
+      pc::pqpp::IterateResult res = connection.iterate(
+          "SELECT coalesce(MAX(priority),$2) AS priority FROM priority_table WHERE "
+          "clientId=$1",
+          params);
+      std::cout << "Client exists so existing value found" << std::endl;
+      std::cout << "Client ID = " << params[0] << " : "
+                << "priority = " << res[0] << "\n";
+   }
+   // Select single client which does not exist
+   {
+      std::cout << "Client does not exist so default value returned\n";
+      std::vector<const char*> params(2);
+      params[0] = "CLIENT-3";
+      params[1] = "45"; // DEFAULT
+
+      pc::pqpp::IterateResult res = connection.iterate(
+          "SELECT coalesce(MAX(priority),$2) AS priority FROM priority_table WHERE "
+          "clientId=$1",
+          params);
+      std::cout << "Client ID = " << params[0] << " : "
+                << "priority = " << res[0] << "\n";
    }
    return EXIT_SUCCESS;
 }
