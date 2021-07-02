f974469042cbe851e788902b11662d7a34b3f8f9
Pratik Chowdhury <pratikc@live.co.uk>
2021-07-02 18:27:43 +0000

    feat: add lexical cast

---
+++ lexical_cast/include/pc/lexical_cast.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdlib>
+#include <string>
+
+#include <pc/templates.hpp>
+
+namespace pc
+{
+   template <typename Param, typename ReturnV>
+   ReturnV lexical_cast(Param)
+   {
+   }
+
+   template <>
+   int lexical_cast<std::string, int>(std::string value)
+   {
+      return std::atoi(value.c_str());
+   }
+   template <>
+   long lexical_cast<std::string, long>(std::string value)
+   {
+      return std::atol(value.c_str());
+   }
+
+   template <typename T>
+   typename enable_if<!is_same<T, std::string>::value, std::string>::type
+       lexical_cast(T value)
+   {
+      std::ostringstream stream;
+      stream << value;
+      return stream.str();
+   }
+} // namespace pc
