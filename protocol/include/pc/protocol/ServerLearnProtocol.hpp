16ef0e01f1b8851c491f8525b653a9ce3ee1f739
Pratik Chowdhury <pratikc@live.co.uk>
2021-07-06 08:11:44 +0000

    feat: use OrderedHashMap for health check

--- protocol/include/pc/protocol/ServerLearnProtocol.hpp
+++ protocol/include/pc/protocol/ServerLearnProtocol.hpp
@@ -5,11 +5,11 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+#include <pc/deadliner/MostRecentTimestamps.hpp>
+#include <pc/network/TCPPoll.hpp>
 #include <pc/protocol/ClientInfo.hpp>
 #include <pc/protocol/Config.hpp>
 #include <pc/protocol/LearnProtocol.hpp>
-
-#include <pc/network/TCPPoll.hpp>
 
 namespace pc
 {
@@ -19,10 +19,15 @@ namespace pc
       {
          typedef std::tr1::unordered_map<int /*Socket*/, ClientInfo> ClientInfos;
          typedef DataQueue<pollfd>::QueueVec::iterator               QueueIterator;
+         typedef deadliner::MostRecentTimestamps<DataQueue<pollfd>::QueueVec>
+             MostRecentTimestamps;
 
          ClientInfos        clientInfos;
          network::TCPPoll   tcpPoll;
          pc::threads::Mutex pollsMutex;
+
+         MostRecentTimestamps mostRecentTimestamps;
+         pc::threads::Mutex   mostRecentTimestampsMutex;
 
          std::size_t size() const
          {
@@ -31,17 +36,27 @@ namespace pc
 
          void closeConnection(QueueIterator it)
          {
-            close(it->fd);
+            int socket = it->fd;
+            close(socket);
             {
                pc::threads::MutexGuard guard(pollsMutex);
+               // Check if the socket exists first of all
+               // If it does not
+               // Do nothing
+               if (clientInfos.find(socket) == clientInfos.end())
+                  return;
 
                std::size_t indexErase = it - tcpPoll.dataQueue.out.begin();
                config->balancer->decPriority(balancerIndex,
-                                             clientInfos[it->fd].deadline.MaxCount());
-               clientInfos.erase(it->fd);
+                                             clientInfos[socket].deadline.MaxCount());
+               clientInfos.erase(socket);
                // Delete current element
                tcpPoll -= indexErase;
             }
+            // {
+            //    pc::threads::MutexGuard guard(mostRecentTimestampsMutex);
+            //    mostRecentTimestamps -= socket;
+            // }
             // Notify user when a File Descriptor goes down
             config->downCallback(balancerIndex);
          }
@@ -60,17 +75,21 @@ namespace pc
             }
             network::buffer buffer(UINT16_MAX);
             NetworkPacket   readPacket = NetworkPacket::Read(*it, buffer, 0);
-            if (readPacket.command == Commands::DownDetect::DownAlive)
             {
+               pc::threads::MutexGuard guard(pollsMutex);
                clientInfos[it->fd].scheduleTermination = false;
-               return;
+            }
+            {
+               pc::threads::MutexGuard guard(mostRecentTimestampsMutex);
+               // Add socket and iterator to current index
+               // Makes removal easy
+               mostRecentTimestamps.insert(it->fd, it);
             }
             if (readPacket.command == Commands::MajorErrors::SocketClosed)
                return closeConnection(it);
             if (readPacket.command != Commands::Send)
                return;
-            clientInfos[it->fd].scheduleTermination = false;
-            NetworkSendPacket const packet          = executeCallback(it->fd, readPacket);
+            NetworkSendPacket const packet = executeCallback(it->fd, readPacket);
             if (packet.command == Commands::Send)
                if (!WritePacket(packet, it))
                   closeConnection(it);
@@ -139,28 +158,37 @@ namespace pc
          {
             if (config != NULL && !config->healthCheckDurationToPerform)
                return;
+            if (mostRecentTimestamps.size() == 0)
+               return;
 
-            for (QueueIterator it = tcpPoll.dataQueue.out.begin();
-                 it != tcpPoll.dataQueue.out.end();
-                 ++it)
-               if (clientInfos[it->fd].deadline.HealthCheckNeeded())
+            std::time_t const now = timer::seconds();
+            for (MostRecentTimestamps::iterator it = mostRecentTimestamps.begin();
+                 it != mostRecentTimestamps.end();)
+            {
+               int socket = it->first;
+               // Input is sorted by ascending order
+               if ((now - it->second.first /*Time then*/) <
+                   config->healthCheckDurationToPerform.maxDurationDifference)
+                  break;
+               // If this is not the cycle to terminate
+               if (clientInfos[socket].scheduleTermination)
                {
-                  // If this is not the cycle to terminate
-                  if (!clientInfos[it->fd].scheduleTermination)
-                  {
-                     // Send Down Check first
-                     NetworkPacket downCheck(Commands::DownDetect::DownCheck);
-                     downCheck.Write(*it, timeout);
-                     clientInfos[it->fd].scheduleTermination = true;
-                  }
-                  // It is enabled
-                  // So if we end up here again
-                  // It means that the Health Check was not reset during
-                  // So no message came
-                  // Hence kill
-                  else
-                     closeConnection(it);
+                  closeConnection(it->second.second);
+                  pc::threads::MutexGuard guard(mostRecentTimestampsMutex);
+                  it = mostRecentTimestamps.removeAndIterate(socket);
                }
+               // It is enabled
+               // So if we end up here again
+               // It means that the Health Check was not reset during
+               // So no message came
+               // Hence kill
+               else
+               {
+                  // Schedule termination
+                  clientInfos[socket].scheduleTermination = true;
+                  ++it;
+               }
+            }
          }
 
          void poll()
