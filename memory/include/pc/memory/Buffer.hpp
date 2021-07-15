90603530b9233406948dd2f384b8026b41763762
Pratik Chowdhury <pratikc@live.co.uk>
2021-07-15 08:46:46 +0000

    feat: add support for Offset within buffer

--- memory/include/pc/memory/Buffer.hpp
+++ memory/include/pc/memory/Buffer.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 namespace pc
 {
    namespace memory
@@ -9,39 +11,53 @@ namespace pc
       {
          T*          arr;
          std::size_t sizeV;
+         std::size_t offset;
 
        public:
-         Buffer(std::size_t size) : arr(new T[size]), sizeV(size) {}
+         Buffer(std::size_t size) : arr(new T[size]), sizeV(size), offset(0)
+         {
+            assert(size > 0);
+         }
          ~Buffer()
          {
             if (arr != NULL)
                delete[] arr;
          }
 
+         void Offset(std::size_t newOffset)
+         {
+            assert(offset < sizeV);
+            offset = newOffset;
+         }
+         std::size_t Offset() const
+         {
+            return offset;
+         }
+
          std::size_t size() const
          {
-            return sizeV;
+            return sizeV - offset;
          }
          T* data()
          {
-            return arr;
+            return arr + offset;
          }
          const T* data() const
          {
-            return arr;
+            return arr + offset;
          }
          T& operator[](std::size_t index)
          {
-            return arr[index];
+            return arr[index + offset];
          }
          T operator[](std::size_t index) const
          {
-            return arr[index];
+            return arr[index + offset];
          }
          T* begin() const
          {
-            return arr;
+            return arr + offset;
          }
       };
-   } // namespace opt
+   } // namespace memory
 } // namespace pc
