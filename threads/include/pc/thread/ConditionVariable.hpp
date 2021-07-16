cfbd0116dc54e4eece9b5af9828fc637f991b387
Pratik Chowdhury <pratikc@live.co.uk>
2021-07-16 05:11:04 +0000

    feat: add Condition variable

---
+++ threads/include/pc/thread/ConditionVariable.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <pthread.h>
+#include <stdexcept>
+
+namespace pc
+{
+   namespace threads
+   {
+      class ConditionVariable
+      {
+       public:
+         pthread_cond_t condition;
+
+         ConditionVariable()
+         {
+            if (pthread_cond_init(&condition, NULL) != 0)
+            {
+               throw std::runtime_error("Mutex init failed");
+            }
+         }
+         template <typename Mutex>
+         bool Wait(Mutex& mutex)
+         {
+            return pthread_cond_wait(&condition, &mutex.mutex) == 0;
+         }
+         bool Signal()
+         {
+            return pthread_cond_signal(&condition) == 0;
+         }
+         bool Broadcast()
+         {
+            return pthread_cond_broadcast(&condition) == 0;
+         }
+
+         ~ConditionVariable()
+         {
+            pthread_cond_destroy(&condition);
+         }
+      };
+   } // namespace threads
+} // namespace pc
